47ea9e2290f4ee50a70f6495d3abd7a9faeeae36
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-04 08:00:58 +0100

    Update seek.js

--- commands/Music/seek.js
+++ commands/Music/seek.js
@@ -18,7 +18,7 @@ module.exports = {
         const queue = await client.distube.getQueue(interaction)
      /*   if (!voiceChannel) {
             return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
-        }*/
+        }
         if (!queue) {
             const queueError = new Discord.MessageEmbed()
                 .setDescription("There is Nothing Playing")
@@ -27,7 +27,7 @@ module.exports = {
         }
         if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
             return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
-        }
+        }*/
         const time = parseInt(args)
         if (!time) return interaction.reply({ content: "Please specify a time | Time in seconds." })
         if (time >= queue.songs[0].duration) return interaction.reply({ content: `Time < \`${queue.songs[0].duration} seconds\`` })

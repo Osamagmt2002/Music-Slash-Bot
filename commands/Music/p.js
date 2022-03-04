65266920f98dad88ca831b88ca4b1745e5657d6e
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-04 07:05:15 +0100

    Update p.js

--- commands/Music/p.js
+++ commands/Music/p.js
@@ -14,14 +14,14 @@ module.exports = {
         const voiceChannel = interaction.member.voice.channel
         const queue = await client.distube.getQueue(interaction)
         const query = interaction.options.get("query").value
-        if (!voiceChannel) {
+     /*   if (!voiceChannel) {
             return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
         }
         if (queue) {
             if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
                 return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
             }
-        }
+        }*/
         await interaction.reply("🔍 **Searching and attempting...**")
         await interaction.editReply("Searching done :ok_hand: ")
         client.distube.play(voiceChannel, query, {

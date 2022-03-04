1494cf13a3dd4aed0003b3db5a375fa37a2dd244
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-04 07:42:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -123,16 +123,16 @@ client.distube
             .addField("**Status**", status(queue).toString())
             .setFooter({ text: `Requested by ${song.user.username}`, iconURL: song.user.avatarURL() })
             .setTimestamp()*/
-        queue.textChannel.send(`:notes: Added ${song.name} (${song.formattedDuration.toString}) to begin playing.`)
+        queue.textChannel.send(`:notes: Added ${song.name} (${song.formattedDuration}) to begin playing.`)
     })
-    .on("addSong", (queue, song) => {
+  /*  .on("addSong", (queue, song) => {
         const embed = new MessageEmbed()
             .setTitle(":ballot_box_with_check: | Added song to queue")
             .setDescription(`\`${song.name}\` - \`${song.formattedDuration}\` - Requested by ${song.user}`)
             .setColor("RANDOM")
             .setTimestamp()
         queue.textChannel.send({ embeds: [embed] })
-    })
+    })*/
     .on("addList", (queue, playlist) => {
         const embed = new MessageEmbed()
             .setTitle(":ballot_box_with_check: | Add list")
@@ -146,21 +146,21 @@ client.distube
         textChannel.send(`An error encountered: ${e}`)
     })
     // .on("finish", queue => queue.textChannel.send("***No more song in queue. Leaving the channel***"))
-    .on("finishSong", queue => {
+   /* .on("finishSong", queue => {
         const embed = new MessageEmbed()
             .setDescription(`:white_check_mark: | Finished playing \`${queue.songs[0].name}\``)
         queue.textChannel.send({ embeds: [embed] })
-    })
-    .on("disconnect", queue => {
+    })*/
+  /*  .on("disconnect", queue => {
         const embed = new MessageEmbed()
             .setDescription(":x: | Disconnected from voice channel")
         queue.textChannel.send({ embeds: [embed] })
-    })
-    .on("empty", queue => {
+    })*/
+  /*  .on("empty", queue => {
         const embed = new MessageEmbed()
             .setDescription(":x: | Channel is empty. Leaving the channel!")
         queue.textChannel.send({ embeds: [embed] })
-    })
+    })*/
     .on("initQueue", (queue) => {
         queue.autoplay = false
         queue.volume = 50
@@ -175,23 +175,3 @@ process.on("SIGINT", () => {
     console.log("\x1b[36m%s\x1b[0m", "SIGINT detected, exiting...")
     process.exit(0)
 })
-// check update repo
-const fetch = require("node-fetch")
-const { version } = require("./version.json")
-console.log("\x1b[33m%s\x1b[0m", `Current version : ${version}`)
-fetch("https://raw.githubusercontent.com/HELLSNAKES/Music-Slash-Bot/main/version.json")
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.version !== version) {
-            console.log("\x1b[32m%s\x1b[0m", "===============================Update Available===================================")
-            console.log("Ver:", data.version)
-            console.log("\x1b[36m%s\x1b[0m", "Check commit : https://github.com/HELLSNAKES/Music-Slash-Bot/commits/main")
-            console.log("\x1b[31m%s\x1b[0m", "Use `npm run updatebot` to update")
-            console.log("\x1b[32m%s\x1b[0m", "==================================================================================")
-        } else {
-            console.log("\x1b[32m%s\x1b[0m", "No Update Available")
-        }
-    })
-    .catch((err) => {
-        console.log("\x1b[31m%s\x1b[0m", err)
-    })

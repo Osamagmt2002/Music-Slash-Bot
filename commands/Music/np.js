53e46d15bc550ef5b7ffda1bb25592d1f5c42fa3
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-05 04:13:25 +0100

    Update np.js

--- commands/Music/np.js
+++ commands/Music/np.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const status = (queue) => `Volume: \`${queue.volume}%\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\` | Filter: \`${queue.filters.join(", ") || "Off"}\``
 module.exports = {
-    name: "nowplay",
+    name: "np",
     description: "Current song playing",
     timeout: 5000,
     run: async (interaction, client) => {
@@ -20,18 +20,10 @@ module.exports = {
             return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
         }*/
         const song = queue.songs[0]
-        const embed = new Discord.MessageEmbed()
-            .setAuthor({ name: "Now Playing", iconURL: "https://raw.githubusercontent.com/HELLSNAKES/Music-Slash-Bot/main/assets/music.gif" })
-            .setDescription(`[${song.name}](${song.url})`)
-            .addField("**Views:**", song.views.toString(), true)
-            .addField("**Like:**", song.likes.toString(), true)
-            .addField("**Duration:**", `${queue.formattedCurrentTime} / ${song.formattedDuration}`)
-            .addField("**Link**", `[Download This Song](${song.streamURL})`)
-            .addField("**Status**", status(queue).toString())
-            .setThumbnail(song.thumbnail)
-            .setColor("RANDOM")
-            .setFooter({ text: `Requested by ${song.user.username}`, iconURL: song.user.avatarURL() })
+      const embed = new Discord.MessageEmbed()
+            .setDescription(`:notes: **Now Playing**`, `\`[${queue.formattedCurrentTime}/${song.formattedDuration}]\``)
+            .setFooter({ text: `${song.user.username}`, iconURL: song.user.avatarURL() })
             .setTimestamp()
-        return interaction.reply({ embeds: [embed] })
+      return interaction.reply({ embeds: [embed] })
     }
 }
